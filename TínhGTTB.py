54b57dbbe5163a6ee8c458710d4e8d1a2c520e1d
huongtiu301027-a11y <huongtiu301027@gmail.com>
2026-05-06 22:05:17 +0700

    Add files via upload

---
+++ TínhGTTB.py
@@ -0,0 +1,7 @@
+# Nhập dãy số cách nhau bởi dấu cách
+day_so = list(map(float, input("Nhập dãy số: ").split()))
+
+trung_binh = sum(day_so) / len(day_so)
+
+print("Giá trị trung bình là:", trung_binh)
+print("Giá trị trung bình là:", trung_binh)
