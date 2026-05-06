082ddf2fb76fb94ee82c435c28fe971831f89495
huongtiu301027-a11y <huongtiu301027@gmail.com>
2026-05-06 21:59:03 +0700

    Add files via upload

---
+++ LN sau 12t.py
@@ -0,0 +1,9 @@
+# Nhập số tiền ban đầu và lãi suất (%/tháng)
+tien_ban_dau = float(input("Nhập số tiền ban đầu: "))
+lai_suat = float(input("Nhập lãi suất (%/tháng): ")) / 100
+
+so_thang = 12
+
+tien_cuoi = tien_ban_dau * (1 + lai_suat) ** so_thang
+
+print("Số tiền sau 12 tháng là:", tien_cuoi)
